471e5a9d4309aa1336844f5370455b544a0df3a7
divyaanshbhilware11 <divyaanshbhilware11@gmail.com>
2024-03-18 10:59:32 +0530

    netlify issues

--- src/main.jsx
+++ src/main.jsx
@@ -36,6 +36,7 @@ const router =  createBrowserRouter ([
         path: 'userdata',
         element : <Userlist />,
        },
+       
        {
         path: 'profileupdate',
         element : <Profileupdate />,
