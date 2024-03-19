d524f5b4c1af832324e77212d75af0dba36689b8
divyaanshbhilware11 <divyaanshbhilware11@gmail.com>
2024-03-19 12:22:01 +0530

    vercel-add

--- src/Constant/Constants.js
+++ src/Constant/Constants.js
@@ -1,5 +1,5 @@
 const CONSTANTS={
-    BASE_URL:'https://internship-backend-iqzbjq7dr-vivekhanda123s-projects.vercel.app/'
+    BASE_URL:'https://internship-backend-1ylle5vpf-vivekhanda123s-projects.vercel.app/'
 }
 
 export default CONSTANTS;
